7406027afa840cff38d957b54da50b0644e3246e
minkyung <alsrud3449@naver.com>
2023-04-17 19:53:58 +0900

    logout 생성

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('sign-up/', views.sign_up_view, name='sign-up'),
     path('log-in/', views.log_in_view, name='log-in'),
+    path('logout/', views.logout, name='logout'),
 ]
