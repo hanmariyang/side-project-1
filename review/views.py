934e13c97bfa3e44f4a071ff5f08525e56d859ce
minkyung <alsrud3449@naver.com>
2023-04-18 19:53:14 +0900

    detail 함수 생성

--- review/views.py
+++ review/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from .models import ReviewModel
 
 # Create your views here.
 def home(request):
@@ -15,3 +16,25 @@ def review(request):
             return render(request, 'review/home.html')
         else:
             return redirect('/log-in')
+        
+def detail(request):
+    if request.method == 'GET': # 요청하는 방식이 GET 방식인지 확인하기
+        user = request.user.is_authenticated  # 사용자가 로그인이 되어있는지 확인
+        if user: #로그인 한 사용자라면
+             return render(request, 'review/detail.html')
+        else: #로그인이 되어 있지 않다면
+             return redirect('/log-in')
+    elif request.method == 'POST': #요청 방식이 POST 일때
+        user = request.user  # 현재 로그인 한 사용자를 불러오기
+        my_review = ReviewModel()
+        my_review.user = user # 모델에 사용자 저장
+        my_review.rating = request.POST.get('rating','')
+        my_review.image =request.POST.get('image','')
+        my_review.purchase_info = request.POST.get('purchase_info','') 
+        my_review.product_info =request.POST.get('product_info','')
+        my_review.size = request.POST.get('size','')
+        my_review.content = request.POST.get('content','')
+        my_review.save()
+        return redirect('/detail')
+    
+        
