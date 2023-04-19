f729c534773b1b8ec511144d8e669430a687212c
minkyung <alsrud3449@naver.com>
2023-04-19 16:02:23 +0900

    delete url 생성

--- review/urls.py
+++ review/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), 
     path('review/', views.review, name='review'),
-    path('detail/', views.detail, name="detail")
+    path('detail/', views.detail, name="detail"),
+    path('detail/delete/<int:id>', views.delete_detail, name='delete'),
 ]
